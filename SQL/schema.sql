ce4e6eaf8d7a9a161d80d723739d366456d86023
Hack Reactor Students <github@hackreactor.com>
2013-07-11 20:42:13 -0700

    Schema design.

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,9 +1,14 @@
-CREATE DATABASE chat;
+/* CREATE DATABASE chat; */
 
 USE chat;
 
+/* CREATE TABLE team2(id int auto_increment, Name varchar(255), City 
+	varchar(255), State char(2), PRIMARY KEY(id)); */
+
 CREATE TABLE messages (
  /* Describe your table here.*/
+ id int auto_increment, username varchar(30), createdAt varchar(30), message varchar(255), PRIMARY KEY(id)
+
 );
 
 /* You can also create more tables, if you need them... */
